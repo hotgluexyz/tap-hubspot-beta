f3a71472b4830baa8531a0dab18d3e07ad1b07e5
Key <keynarafaelp@gmail.com>
2024-06-06 11:18:21 -0500

    not backoff on 400

--- tap_hubspot_beta/client_base.py
+++ tap_hubspot_beta/client_base.py
@@ -142,7 +142,7 @@ class hubspotStream(RESTStream):
 
     def validate_response(self, response: requests.Response) -> None:
         """Validate HTTP response."""
-        if 500 <= response.status_code < 600 or response.status_code in [429, 401, 104, 400]:
+        if 500 <= response.status_code < 600 or response.status_code in [429, 401, 104]:
             msg = (
                 f"{response.status_code} Server Error: "
                 f"{response.reason} for path: {self.path}"
