a2bb70171892a4feb81cb766b961f5ed6cb9b84f
Otavio Trentin <otavio_trentin@hotmail.com>
2026-02-26 12:34:33 -0300

    remove redundant get_next_page_token override in InboxesStream

--- tap_hubspot_beta/streams.py
+++ tap_hubspot_beta/streams.py
@@ -3718,11 +3718,3 @@ class InboxesStream(hubspotV3Stream):
     ).to_dict()
 
     primary_keys = ["id"]
-
-    def get_next_page_token(
-        self, response: requests.Response, previous_token: Optional[Any]
-    ) -> Optional[Any]:
-        """Return a token for identifying next page or None if no more pages."""
-        if not response.json().get("results"):
-            return None
-        return super().get_next_page_token(response, previous_token)
