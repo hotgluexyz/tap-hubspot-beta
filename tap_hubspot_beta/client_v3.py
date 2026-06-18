f3b9ac94d9dc4d174214843ceb28407d38bd9787
Otavio Trentin <otavio_trentin@hotmail.com>
2026-06-18 11:18:35 -0400

    HGI-10478: remove context from get_estimated_record_count

--- tap_hubspot_beta/client_v3.py
+++ tap_hubspot_beta/client_v3.py
@@ -148,12 +148,12 @@ class hubspotV3SearchStream(hubspotStream):
 
         return response.get('total')
 
-    def get_estimated_record_count(self, context: Optional[dict] = None) -> Optional[int]:
+    def get_estimated_record_count(self) -> Optional[int]:
         if self.config.get("emit_estimated_record_totals_snapshot", True):
-            self._write_starting_replication_value(context)
-            starting_time = self.get_starting_time(context)
+            self._write_starting_replication_value(context=None)
+            starting_time = self.get_starting_time(context=None)
             end_time = self.get_end_time()
-            return self.get_time_bucket_size(context, starting_time, end_time)
+            return self.get_time_bucket_size(None, starting_time, end_time)
         else:
             return None
 
